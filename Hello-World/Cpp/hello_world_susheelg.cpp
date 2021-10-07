98ac3162503472adcdc703f4ea078151f1afe856
Susheel Gounder <susheel.g@peopleinteractive.in>
2021-10-07 20:27:53 +0530

    Added hello world

---
+++ Hello-World/Cpp/hello_world_susheelg.cpp
@@ -0,0 +1,14 @@
+/*
+LANGUAGE: cpp
+AUTHOR: Susheel G
+GITHUB: https://github.com/susheelg
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello, World!";
+    return 0;
+}
